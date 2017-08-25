6bae6d927cce5d3f5e64691c60b5a0fd14ec073a
michel adriano medeiros <michel adriano medeiros>
2017-08-25 12:29:56 -0300

    first commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Procura no Google</p>
+        <h2>Missão e Visão</h2>
+        <p>Aprender React e a loucura do Redux</p>
+        <h2>Imprensa</h2>
+        <p>michel.medeiros@ti.rc.sp.gov.br</p>
+        
+    </div>
+)
